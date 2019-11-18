3f8c3cf640f29a2c6d0b81218caa31d89d355285
Henri Evjen <henrievjen@gmail.com>
2019-11-17 21:18:48 -0600

    master clean

--- views/main/Main.tsx
+++ views/main/Main.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { ImageBackground } from 'react-native';
 
 export class Main extends Component {
     render() {
     const backgroundImage = require('./main-background.png');
 
     return (
-        <ImageBackground source={backgroundImage} style={{width: '100%', height: '100%'}}><View><Text>Servings Calculator</Text></View></ImageBackground>
+        <ImageBackground source={backgroundImage} style={{width: '100%', height: '100%'}}></ImageBackground>
     );
 }
 }
