5f538070edbbbf1a53a2eb4953ad7adf31fbd23a
Henri Evjen <henrievjen@gmail.com>
2019-12-23 16:53:37 -0600

    added ability to add ingredients, each ingredient is stored in state

--- views/main/Main.tsx
+++ views/main/Main.tsx
@@ -1,79 +1,130 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, Image, StyleSheet, Picker, TextInput } from 'react-native';
+import { View, ScrollView, Text, ImageBackground, Image, StyleSheet, Picker, TextInput, TouchableOpacity } from 'react-native';
 
 export class Main extends Component {
   state = {
-    chosenIngredient: '',
-    chosenMeasure: ''
+    ingredients: [
+      {
+        ingredient: 'apples',
+        measure: 'count',
+        quantity: 1
+      }
+    ]
   };
 
   render() {
-  const backgroundImage = require('./main-background.png');
-  const plusSymbol = require('./images/plus-symbol.png');
-  const styles = StyleSheet.create({
-    backgroundImage: {
-      width: '100%',
-      height: '100%'
-    },
-    headContainer: {
-      width: '100%',
-      height: '20%',
-      top: '10%'
-    },
-    headBox: {
-      width: '70%',
-      height: 'auto',
-      backgroundColor: 'white',
-      borderRadius: 10,
-      paddingVertical: 25,
-      alignSelf: 'center',
-    },
-    ingredientsContainer: {
-      width: '100%',
-      height: '60%',
-      marginTop: 60
-    },
-    ingredient: {
-      width: '70%',
-      height: 70,
-      backgroundColor: '#E1E6FF',
-      borderRadius: 50,
-      alignSelf: 'center',
-      flexDirection: "row"
-    },
-    addIngredient: {
-      width: '70%',
-      height: 70,
-      alignSelf: 'center',
-      marginTop: 10
-    },
-    pickerIngredient: {
-      width: '25%',
-      height: '100%',
-      backgroundColor: '#E1E6FF',
-      marginLeft: '10%'
-    },
-    pickerMeasure: {
-      width: '45%',
-      height: '100%',
-      backgroundColor: '#E1E6FF'
-    },
-    quantityInput: {
-      width: '20%',
-      height: '100%'
-    },
-    plusSymbol: {
-      height: 60,
-      width: 60,
-      alignSelf: 'flex-end'
-    },
-    text: {
-      fontSize: 40,
-      fontWeight: 'bold',
-      color: 'black',
-      alignSelf: 'center'
+    const backgroundImage = require('./main-background.png');
+    const plusSymbol = require('./images/plus-symbol.png');
+
+    const styles = StyleSheet.create({
+      backgroundImage: {
+        width: '100%',
+        height: '100%'
+      },
+      headContainer: {
+        width: '100%',
+        height: '20%',
+        top: '10%'
+      },
+      headBox: {
+        width: '70%',
+        height: 'auto',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        paddingVertical: 25,
+        alignSelf: 'center',
+      },
+      ingredientsContainer: {
+        width: '100%',
+        height: '40%',
+        marginTop: 60
+      },
+      ingredient: {
+        width: '70%',
+        height: 70,
+        backgroundColor: '#E1E6FF',
+        borderRadius: 50,
+        alignSelf: 'center',
+        flexDirection: "row",
+        marginBottom: 10
+      },
+      addIngredient: {
+        width: '70%',
+        height: 70,
+        alignSelf: 'center',
+        marginTop: 10
+      },
+      pickerIngredient: {
+        width: '25%',
+        height: '100%',
+        backgroundColor: '#E1E6FF',
+        marginLeft: '10%'
+      },
+      pickerMeasure: {
+        width: '45%',
+        height: '100%',
+        backgroundColor: '#E1E6FF'
+      },
+      quantityInput: {
+        width: '20%',
+        height: '100%'
+      },
+      plusSymbol: {
+        height: 60,
+        width: 60,
+        alignSelf: 'flex-end'
+      },
+      notes: {
+        width: '70%',
+        height: 70,
+        alignSelf: 'center',
+        backgroundColor: 'white',
+        borderRadius: 5,
+        textAlignVertical: 'top',
+        padding: 5
+      },
+      saveButton: {
+        marginTop: '20%',
+        width: '50%',
+        height: '8%',
+        borderRadius: 10,
+        backgroundColor: '#50AD6A',
+        alignSelf: 'center'
+      },
+      text: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'black',
+        alignSelf: 'center'
+      }
+    });
+
+    const fullIngredient = this.state.ingredients.map((data, id) => {
+      return (
+        <View style={styles.ingredient} key={id}>
+          <Picker style={styles.pickerIngredient} selectedValue={data.ingredient}>
+            <Picker.Item label="Apples" value="apples" />
+            <Picker.Item label="Eggs" value="eggs" />
+            <Picker.Item label="Milk" value="milk" />
+            <Picker.Item label="Sugar" value="sugar" />
+          </Picker>
+          <Picker style={styles.pickerMeasure} selectedValue={data.measure}>
+            <Picker.Item label="Count" value="count" />
+            <Picker.Item label="Cups" value="cups" />
+            <Picker.Item label="Ounces" value="ounces" />
+            <Picker.Item label="Tablespoons" value="tablespoons" />
+            <Picker.Item label="Teaspoons" value="teaspoons" />
+          </Picker>
+          <TextInput style={styles.quantityInput} keyboardType={'numeric'} defaultValue={"1"} selectTextOnFocus />
+        </View>
+      )
+    })
+
+    const addIngredient = () => {
+      let tempState = this.state.ingredients;
+      tempState.push({ingredient: 'apples', measure: 'count', quantity: 1});
+      this.setState(tempState);
     }
-  });
 
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
@@ -82,28 +133,17 @@ export class Main extends Component {
             <Text style={styles.text}>Recipe</Text>
           </View>
         </View>
-        <View style={styles.ingredientsContainer}>
-          <View style={styles.ingredient}>
-            <Picker style={styles.pickerIngredient} selectedValue={this.state.chosenIngredient} onValueChange={(itemValue) => this.setState({chosenIngredient: itemValue})}>
-              <Picker.Item label="Apples" value="apples" />
-              <Picker.Item label="Eggs" value="eggs" />
-              <Picker.Item label="Milk" value="milk" />
-              <Picker.Item label="Sugar" value="sugar" />
-            </Picker>
-            <Picker style={styles.pickerMeasure} selectedValue={this.state.chosenMeasure} onValueChange={(itemValue) => this.setState({chosenMeasure: itemValue})}>
-              <Picker.Item label="Count" value="count" />
-              <Picker.Item label="Cups" value="cups" />
-              <Picker.Item label="Ounces" value="ounces" />
-              <Picker.Item label="Tablespoons" value="tablespoons" />
-              <Picker.Item label="Teaspoons" value="teaspoons" />
-            </Picker>
-            <TextInput style={styles.quantityInput} keyboardType={'numeric'} defaultValue={"1"} selectTextOnFocus />
-          </View>
-          <View style={styles.addIngredient}>
+        <ScrollView style={styles.ingredientsContainer}>
+            {fullIngredient}
+          <TouchableOpacity style={styles.addIngredient} onPress={addIngredient}>
             <Image source={plusSymbol} style={styles.plusSymbol} />
-          </View>
-        </View>
+          </TouchableOpacity>
+        </ScrollView>
+        <TextInput style={styles.notes} placeholder={"Notes..."} />
+        <TouchableOpacity style={styles.saveButton}>
+          <Text style={{paddingVertical: '5%', textAlign: 'center', textAlignVertical: 'center', color: 'white', fontSize: 30}}>Save</Text>
+        </TouchableOpacity>
       </ImageBackground>
     );
-}
+  }
 }
